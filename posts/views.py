cb34955249a7c439a42daf5a0d80e75736bb65e2
Chung Nguyen <zrmedia9@gmail.com>
2018-06-24 10:07:38 +0700

    SlugField update

--- posts/views.py
+++ posts/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.http import HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404, redirect
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .forms import PostForm
 from .models import Post
@@ -21,8 +21,8 @@ def post_create(request):
     return render(request, "post_form.html", context)
 
 
-def post_detail(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_detail(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     context = {
         "title": instance.title,
         "instance": instance,
@@ -31,9 +31,8 @@ def post_detail(request, id=None):
 
 
 def post_list(request):
-    queryset_list = Post.objects.all() #.order_by("-timestamp")
-
-    paginator = Paginator(queryset_list, 5)  # Show 25 contacts per page
+    queryset_list = Post.objects.all()  # .order_by("-timestamp")
+    paginator = Paginator(queryset_list, 10)  # Show 25 contacts per page
     page_request_var = "page"
     page = request.GET.get(page_request_var)
     try:
@@ -53,8 +52,8 @@ def post_list(request):
     return render(request, "post_list.html", context)
 
 
-def post_update(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_update(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     form = PostForm(request.POST or None, request.FILES or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
@@ -70,8 +69,9 @@ def post_update(request, id=None):
     return render(request, "post_form.html", context)
 
 
-def post_delete(request, id=None):
-    instance = get_object_or_404(Post, id=id)
+def post_delete(request, slug=None):
+    instance = get_object_or_404(Post, slug=slug)
     instance.delete()
     messages.success(request, "Successfully deleted")
     return redirect("posts:list")
+
